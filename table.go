256b9643a59499426fbff7dbfb6d130177d8bf41
Pedro Coelho <punnie@ethereal.io>
2023-08-20 15:43:58 +0100

    Allow for omitting the header when printing

--- table.go
+++ table.go
@@ -50,6 +50,9 @@ var (
 
 	// DefaultWidthFunc specifies the default WidthFunc for calculating column widths
 	DefaultWidthFunc WidthFunc = utf8.RuneCountInString
+
+	// DefaultPrintHeaders specifies if headers should be printed
+	DefaultPrintHeaders = true
 )
 
 // Formatter functions expose a fmt.Sprintf signature that can be used to modify
@@ -121,6 +124,7 @@ type Table interface {
 	WithPadding(p int) Table
 	WithWriter(w io.Writer) Table
 	WithWidthFunc(f WidthFunc) Table
+	WithPrintHeaders(b bool) Table
 
 	AddRow(vals ...interface{}) Table
 	SetRows(rows [][]string) Table
@@ -138,6 +142,7 @@ func New(columnHeaders ...interface{}) Table {
 	t.WithHeaderFormatter(DefaultHeaderFormatter)
 	t.WithFirstColumnFormatter(DefaultFirstColumnFormatter)
 	t.WithWidthFunc(DefaultWidthFunc)
+	t.WithPrintHeaders(DefaultPrintHeaders)
 
 	for i, col := range columnHeaders {
 		t.header[i] = fmt.Sprint(col)
@@ -152,6 +157,7 @@ type table struct {
 	Padding              int
 	Writer               io.Writer
 	Width                WidthFunc
+	PrintHeaders         bool
 
 	header []string
 	rows   [][]string
@@ -188,6 +194,11 @@ func (t *table) WithWriter(w io.Writer) Table {
 
 func (t *table) WithWidthFunc(f WidthFunc) Table {
 	t.Width = f
+	return t
+}
+
+func (t *table) WithPrintHeaders(b bool) Table {
+	t.PrintHeaders = b
 	return t
 }
 
@@ -230,7 +241,10 @@ func (t *table) Print() {
 	format := strings.Repeat("%s", len(t.header)) + "\n"
 	t.calculateWidths()
 
-	t.printHeader(format)
+	if t.PrintHeaders {
+		t.printHeader(format)
+	}
+
 	for _, row := range t.rows {
 		t.printRow(format, row)
 	}
